bf40d9053bb29d093a3d1e079b63d117d8af2d55
Ryan Brookie <129629014+Buddy-Smile@users.noreply.github.com>
2023-12-16 14:07:20 -0700

    Add files via upload

---
+++ project1/src/project1/program1.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class program1 {
+
+	public static void main(String[] args) {
+		System.out.println("First name: Ryan");
+        System.out.println("Last name: Brookie");
+        System.out.println("Street address: 123 Denver St");
+        System.out.println("City: Denver");
+        System.out.println("Zip code: 80127");
+
+
+	}
+
+}
